97d3d5eb64372f5427c3e620d1025e743cc5f594
tsihnavy99 <70021587+tsihnavy99@users.noreply.github.com>
2023-09-14 14:17:45 +0900

    add path /

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
-import { HashRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Link, useLocation } from 'react-router-dom'
 import SignUp from './pages/SignUp';
 import SignIn from './pages/SignIn';
 import ToDo from './pages/ToDo';
+import './App.css';
 
 function App() {
+  const Main = () => {
+    return (
+      <div className="main-container">
+        <div className="content-container">
+          <ul>
+            <li><Link to="/signup">회원가입</Link></li>
+            <li><Link to="/signin">로그인</Link></li>
+            <li><Link to="/todo">ToDo</Link></li>
+          </ul>
+        </div>
+      </div>
+    )
+  }
+
   return (
-    <HashRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
+        <Route path="/" element={<Main/>} />
         <Route path="/signup" element={<SignUp/>} />
         <Route path="/signin" element={<SignIn/>} />
         <Route path="/todo" element={<ToDo/>} />
       </Routes>
-    </HashRouter>
+        
+    </BrowserRouter>
   );
 }
 
