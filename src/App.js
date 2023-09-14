546fb4b0aad4dba4964b75a0368f2d595f9e1f9c
tsihnavy99 <70021587+tsihnavy99@users.noreply.github.com>
2023-09-14 11:39:36 +0900

    router update

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import SignUp from './pages/SignUp';
 import SignIn from './pages/SignIn';
 import ToDo from './pages/ToDo';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/signup" element={<SignUp/>} />
         <Route path="/signin" element={<SignIn/>} />
         <Route path="/todo" element={<ToDo/>} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
