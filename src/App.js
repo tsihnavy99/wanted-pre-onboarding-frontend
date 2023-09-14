4027187af550967b779052e39b86596d833d547e
tsihnavy99 <70021587+tsihnavy99@users.noreply.github.com>
2023-09-14 17:08:11 +0900

    update for publish

--- src/App.js
+++ src/App.js
@@ -1,35 +1,19 @@
-import { BrowserRouter, Routes, Route, Link, useLocation } from 'react-router-dom'
-import SignUp from './pages/SignUp';
-import SignIn from './pages/SignIn';
-import ToDo from './pages/ToDo';
+import { Link } from 'react-router-dom'
+
 import './App.css';
 
 function App() {
-  const Main = () => {
-    return (
-      <div className="main-container">
-        <div className="content-container">
-          <ul>
-            <li><Link to="/signup">회원가입</Link></li>
-            <li><Link to="/signin">로그인</Link></li>
-            <li><Link to="/todo">ToDo</Link></li>
-          </ul>
-        </div>
-      </div>
-    )
-  }
-
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <Routes>
-        <Route path="/" element={<Main/>} />
-        <Route path="/signup" element={<SignUp/>} />
-        <Route path="/signin" element={<SignIn/>} />
-        <Route path="/todo" element={<ToDo/>} />
-      </Routes>
-        
-    </BrowserRouter>
-  );
+    <div className="main-container">
+      <div className="content-container">
+        <ul>
+          <li><Link to="/signup">회원가입</Link></li>
+          <li><Link to="/signin">로그인</Link></li>
+          <li><Link to="/todo">ToDo</Link></li>
+        </ul>
+      </div>
+    </div>
+  )
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import SignUp from './pages/SignUp';
+import SignIn from './pages/SignIn';
+import ToDo from './pages/ToDo';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <Routes>
+        <Route path="/" element={<App/>} />
+        <Route path="/signup" element={<SignUp/>} />
+        <Route path="/signin" element={<SignIn/>} />
+        <Route path="/todo" element={<ToDo/>} />
+      </Routes>
+    </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
